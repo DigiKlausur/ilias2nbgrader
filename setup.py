a5dcaed632dc25d27ab25604e4b0cdca1d6b9ab8
Tim Metzler <tim.metzler@h-brs.de>
2020-01-16 16:58:55 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='ilias2nbgrader',
+    version='0.1',
+    description='Exchange submissions and feedbacks between ILIAS and nbgrader',
+    author='Tim Metzler',
+    author_email='tim.metzler@h-brs.de',
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=[
+        "fuzzywuzzy"
+    ],
+    include_package_data = True,
+    zip_safe=False
+)
