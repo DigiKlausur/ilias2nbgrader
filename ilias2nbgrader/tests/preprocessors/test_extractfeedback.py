fa58334eb66f5c8309d3428ab6329d6d65c8096c
Tim Metzler <tim.metzler@h-brs.de>
2020-01-24 16:47:45 +0100

    Add tests for ExtractFeedback

---
+++ ilias2nbgrader/tests/preprocessors/test_extractfeedback.py
@@ -0,0 +1,23 @@
+import unittest
+import os
+from shutil import rmtree
+from .base import TestsBase
+from ilias2nbgrader.preprocessors import ExtractFeedback
+
+class TestPreprocessor(TestsBase):
+
+    def setUp(self):
+        super().setUp()
+        self.resources['course_dir'] = os.path.join(self.file_path, 'feedback_course')
+
+    def test_process_student(self):
+        path, resources = ExtractFeedback().preprocess(self.tmp_path, self.resources)
+        assert os.path.exists(self.tmp_path)
+        print(path)
+
+        for student in self.students:
+            assert os.path.exists(os.path.join(path, student))
+        assert os.path.exists(os.path.join(path, 'student5'))      
+
+if __name__ == '__main__':
+    unittest.main()
