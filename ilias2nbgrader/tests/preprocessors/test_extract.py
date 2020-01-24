072b634265814199acb54f746ae34f443e298083
Tim Metzler <tim.metzler@h-brs.de>
2020-01-24 14:13:42 +0100

    Use base class for tests

--- ilias2nbgrader/tests/preprocessors/test_extract.py
+++ ilias2nbgrader/tests/preprocessors/test_extract.py
@@ -3,40 +3,29 @@ import sys
 import os
 from shutil import rmtree
 from ilias2nbgrader.preprocessors import Extract
+from .base import TestsBase
 
-class TestExtract(unittest.TestCase):
-
-    def setUp(self):
-        self.base_path = os.path.normpath('ilias2nbgrader/tests/preprocessors')
-        self.file_path = os.path.join(self.base_path, 'files')
-        self.course_dir = os.path.join(self.file_path, 'test_course')
-        self.resources = {
-            'course_dir': self.course_dir,
-            'assignment': 'assignment_one',
-            'path': 'tmp',
-            'submission_zip': os.path.join(self.file_path, 'submissions.zip')
-        }
+class TestExtract(TestsBase):
 
     def test_paths(self):
         preprocessor = Extract()
-        path, resources = preprocessor.preprocess('tmp', self.resources)
+        path, resources = preprocessor.preprocess(self.tmp_path, self.resources)
 
-        assert os.path.exists('tmp/extracted')
-        assert os.path.exists('tmp/extracted/student1')
-        assert os.path.exists('tmp/extracted/student2')
-        assert os.path.exists('tmp/extracted/student3')
-        assert os.path.exists('tmp/extracted/student4')
+
+        assert os.path.exists(os.path.join(self.tmp_path, preprocessor.extract_path))
+        for student in ['student1', 'student2', 'student3', 'student4']:
+            assert os.path.exists(os.path.join(self.tmp_path, preprocessor.extract_path, student))
 
     def test_tmp_folders(self):
         preprocessor = Extract()
         self.resources['tmp_folders'] = set()
-        path, resources = preprocessor.preprocess('tmp', self.resources)
+        path, resources = preprocessor.preprocess(self.tmp_path, self.resources)
 
-        assert 'tmp/extracted' in resources['tmp_folders']
+        assert os.path.join(self.tmp_path, preprocessor.extract_path) in resources['tmp_folders']
 
 
     def tearDown(self):
-        rmtree('tmp')
+        rmtree(self.tmp_path)
 
 if __name__ == '__main__':
     unittest.main()
