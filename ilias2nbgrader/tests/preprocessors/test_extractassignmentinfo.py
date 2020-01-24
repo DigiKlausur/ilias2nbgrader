1209349e52e2775ff48302549f5f21db7f976a5f
Tim Metzler <tim.metzler@h-brs.de>
2020-01-24 14:39:34 +0100

    Extend base tests

--- ilias2nbgrader/tests/preprocessors/test_extractassignmentinfo.py
+++ ilias2nbgrader/tests/preprocessors/test_extractassignmentinfo.py
@@ -1,16 +1,9 @@
 import unittest
-import sys
+import os
+from .base import TestsBase
 from ilias2nbgrader.preprocessors import ExtractAssignmentInfo
 
-class TestExtractAssignmentInfo(unittest.TestCase):
-
-    def setUp(self):
-        self.resources = {
-            'course_dir': 'ilias2nbgrader/tests/preprocessors/files/test_course',
-            'assignment': 'assignment_one',
-            'path': 'tmp',
-            'submission_zip': 'files/submissions.zip'
-        }
+class TestExtractAssignmentInfo(TestsBase):
 
     def test_source_notebooks(self):
         preprocessor = ExtractAssignmentInfo()
@@ -23,16 +16,13 @@ class TestExtractAssignmentInfo(unittest.TestCase):
     def test_extra_files(self):
         preprocessor = ExtractAssignmentInfo()
         path, resources = preprocessor.preprocess('', self.resources)
-        print(resources)
 
         assert 'extra_files' in resources
         assert 'base_path' in resources['extra_files']
         assert 'files' in resources['extra_files']
-        assert resources['extra_files']['base_path'] == 'ilias2nbgrader/tests/preprocessors/files/test_course/source/assignment_one'
+        assert resources['extra_files']['base_path'] == os.path.join(self.course_dir, 'source', self.assignment)
         assert len(resources['extra_files']['files']) == 1
         assert 'data/file.txt' in resources['extra_files']['files']
 
 if __name__ == '__main__':
     unittest.main()
-
-
