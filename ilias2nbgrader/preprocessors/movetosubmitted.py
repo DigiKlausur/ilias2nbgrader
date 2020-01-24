ed594345394bdfcb4ba1a0d537cb6979e59526a3
Tim Metzler <tim.metzler@h-brs.de>
2020-01-24 22:50:53 +0100

    Fix copying in MoveToSubmitted

--- ilias2nbgrader/preprocessors/movetosubmitted.py
+++ ilias2nbgrader/preprocessors/movetosubmitted.py
@@ -1,7 +1,7 @@
 import os
 from .preprocessor import Preprocessor
 from traitlets import Unicode
-from shutil import copytree
+from shutil import copytree, copy
 
 class MoveToSubmitted(Preprocessor):
 
@@ -9,10 +9,21 @@ class MoveToSubmitted(Preprocessor):
     
     def __init__(self):
         super(MoveToSubmitted, self).__init__()
+
+    def copyfiles(self, src, dst):
+        for root, dirs, files in os.walk(src):
+            dst_root = os.path.relpath(root, start=src)
+            for file in files:
+                os.makedirs(os.path.join(dst, dst_root), exist_ok=True)
+                copy(os.path.join(root, file), os.path.join(dst, dst_root, file))
+            for d in dirs:
+                os.makedirs(os.path.join(dst, root, d), exist_ok=True)
             
     def preprocess(self, path, resources):
         self.src = path
         self.dst = os.path.join(resources['course_dir'], self.directory)
-        os.makedirs(self.directory, exist_ok=True)
-        copytree(self.src, self.dst)
+        
+        os.makedirs(self.dst, exist_ok=True)       
+
+        self.copyfiles(self.src, self.dst)
         return self.dst, resources
