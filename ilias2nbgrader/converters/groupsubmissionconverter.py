6d182db641034d3acd9430a8e46abcd0fc076f08
Tim Metzler <tim.metzler@h-brs.de>
2020-04-28 17:45:47 +0200

    Create converter for groups submissions

---
+++ ilias2nbgrader/converters/groupsubmissionconverter.py
@@ -0,0 +1,22 @@
+from traitlets.config import LoggingConfigurable, Config
+from traitlets import List, Unicode
+from .converter import Converter
+from ..preprocessors import *
+
+class GroupSubmissionConverter(Converter):
+    
+    preprocessors = List([
+        ExtractAssignmentInfo,
+        Extract,
+        CreateFolderStructure,
+        RenameNotebooks,
+        AddExtraFiles,
+        RestructureSubmission,
+        RenameSubmission,
+        GroupSubmissions,
+        MoveToSubmitted,
+        DeleteTempFolders
+    ], help='List of preprocessors for the converter')
+    
+    def __init__(self, config=None):
+        super(GroupSubmissionConverter, self).__init__(config=config)
