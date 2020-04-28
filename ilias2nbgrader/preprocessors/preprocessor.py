004dfa85ccf94b8dad1bb679f463175d29e5cfd5
Tim Metzler <tim.metzler@h-brs.de>
2020-04-28 18:00:34 +0200

    Refactor preprocessor

--- ilias2nbgrader/preprocessors/preprocessor.py
+++ ilias2nbgrader/preprocessors/preprocessor.py
@@ -7,8 +7,7 @@ from traitlets import Unicode
 
 class Preprocessor(LoggingConfigurable):
     
-    src = Unicode('', help='Source path to read from')
-    dst = Unicode('', help='Destination path to write to')
+    directory = Unicode('', help='Source path to read from')
     logname = Unicode('converter.log', help='Name of the logfile')
     
     def __init__(self):
@@ -35,7 +34,7 @@ class Preprocessor(LoggingConfigurable):
         
     def preprocess(self, path, resources):
         self.src = path
-        self.dst = path
+        self.dst = os.path.join(resources['path'], self.directory)
         students = [os.path.split(g)[-1] for g in glob.glob(os.path.join(self.src, '*'))]
         for student in students:
             self.preprocess_student(student, resources)
