913c8ebf7af1eb2efb7ec252ea92869d07eb4a5d
Tim Metzler <33530562+tmetzl@users.noreply.github.com>
2020-04-29 12:36:31 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as f:
 
 setup(
     name='ilias2nbgrader',
-    version='0.4.0',
+    version='0.4.1',
     license='MIT',
     url='https://github.com/DigiKlausur/ilias2nbgrader',
     description='Exchange submissions and feedbacks between ILIAS and nbgrader',
